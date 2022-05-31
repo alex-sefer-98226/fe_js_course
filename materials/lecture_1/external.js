6063882722c9899ab884d22bfd2ce47f9dc164f0
Oleksandr Sefer <oleksandr.sefer@pm.bet>
2022-05-31 21:37:58 +0300

    added lesson 1 materials

---
+++ materials/lecture_1/external.js
@@ -0,0 +1,256 @@
+var variable;
+var count;
+// Number
+
+// primitives
+
+// console.log("some string" / "10");
+
+// console.log(typeof NaN); // !
+
+// console.log(42 / 0);
+// console.log(typeof 10);
+// console.log(typeof "10");
+// console.log(typeof true);
+
+// String
+
+// console.log("324.5");
+// console.log("hi");
+// console.log("s", "s", `s`); // template string
+// console.log("alksdla", "lasndklajsndlan", `alskdlajsndjn`);
+
+// Boolean
+
+// true - 1 - Истина
+// false - 0 - Ложь
+
+// console.log(5 > 10);
+// console.log(5 < 10);
+// console.log(variable);
+
+// variable = 45 + 32;
+// console.log(variable);
+
+// variable = "Hi, I'm learning variables";
+// console.log(variable);
+
+// variable = 0 < 55;
+// console.log(variable);
+
+// variable = null;
+// console.log(variable);
+
+// var login = "test";
+// var password = "test_password";
+
+// var myObj = {
+//   login: "myTestLogin",
+//   password: "myTestPassword",
+// };
+
+// console.log(myObj);
+
+// console.log(myObj.login, myObj.password);
+
+// var a = 5;
+// console.log("Variable a: ", a);
+
+// var b = a;
+// console.log("Variable b: ", b);
+
+// b = 7;
+// console.log("========================");
+// console.log("Variable a & b: ", a, b);
+
+// console.log("========================");
+
+// var myObj = {
+//   login: "myTestLogin",
+//   password: "myTestPassword",
+// };
+
+// console.log("MyObj", myObj);
+
+// var secondObj = myObj;
+// console.log("Second Object", secondObj);
+
+// console.log("========================");
+// console.log("========================");
+// secondObj.login = "mySecondObjectLogin";
+
+// console.log("Second Object", secondObj);
+// console.log("MyObj", myObj);
+
+// console.log(typeof NaN);
+// console.log(typeof null);
+
+// const age = 20;
+
+// var ourVariable; // declaration
+// ourVariable = "Vasya"; // initializing
+
+// let programingLanguage = "javascript";
+// console.log("java" + "script");
+
+// console.log(programingLanguage + " " + "<3");
+
+// let globalCount = 0;
+// let counter = 0;
+// {
+//   let localCount = 1;
+//   let counter = 3;
+//   console.log(counter);
+// }
+// console.log(counter);
+// console.log(globalCount); // 0
+// console.log(localCount); // error
+
+// let x = 3;
+// let x = 5;
+
+// var a = 3;
+// var a = 5;
+
+// var globalCount = 0;
+// {
+//   var localCount = 1;
+// }
+// console.log(globalCount); // 0
+// console.log(localCount);
+
+// count = 5;
+
+// console.log(count);
+
+// let userName = "Alex";
+// let userName = "John"; //ошибка, переменная с таким именем уже существует
+
+// let userName = "Alex";
+
+// let username = "John";
+
+// console.log(userName, username);
+
+// let a = 2;
+
+// console.log(a);
+
+// =, +=, -=, *=, /=
+
+// a += 6; // a= a+6
+// console.log(a);
+
+// a -= 6; // a = a - 6
+// console.log(a);
+
+// a *= 4; // a = a * 4;
+// console.log(a);
+
+// a /= 6; // a = a / 6;
+// console.log(a);
+
+// var variable = 3;
+// console.log(variable % 2 == 1); // Является ли число непарным
+// console.log(variable % 2 == 0); // Является ли число парным
+
+// console.log(1435 % 100);
+// console.log(2 < 3);
+// console.log(3 > 2);
+// console.log(3 == 2);
+
+// console.log("2" == 2); // сравнение с привидением типов
+// console.log("2" === 2); // сравнение с учётом типа переменных/значений
+
+// || &&
+
+// var x = false; // 0
+// var y = false;
+
+// // ||
+
+// console.log(x || y);
+
+// x = true; // 1
+// y = false;
+
+// console.log(x || y);
+
+// x = false;
+// y = true;
+
+// console.log(x || y);
+
+// x = true;
+// y = true;
+
+// console.log(x || y);
+
+// // &&
+// var x = false; // 0
+// var y = false;
+
+// console.log("=========================");
+// // &&
+
+// console.log(x && y);
+
+// x = true; // 1
+// y = false;
+
+// console.log(x && y);
+
+// x = false;
+// y = true;
+
+// console.log(x && y);
+
+// x = true;
+// y = true;
+
+// // console.log(x && y);
+// let zero = 0;
+
+// console.log(++zero);
+// console.log(zero);
+
+// zero--; // postfix
+// --zero; // prefix
+
+// console.log(2 != 3);
+
+// console.log(2 !== "2");
+
+// console.log(3 <= 3);
+
+// console.log(3 <= 2);
+
+// console.log(5 >= 5);
+
+// const num = 5;
+
+// console.log(num < 10 && num > 2);
+
+// const x = 3;
+
+// console.log(x <= 5 && x >= 20);
+
+// console.log(!true);
+// console.log(!false);
+
+// console.log(!(3 < 5));
+
+// console.log(!(3 > 5));
+
+// const defaultValue = 10;
+
+// let userValue = 35;
+
+// let result = userValue || defaultValue || true || false;
+// console.log(result);
+
+// let userAge = 35;
+
+// let isAllowed = userAge >= 18 ? "Вам разрешен вход" : "Извините, покиньте сайт";
+
+// console.log(isAllowed);
